9893cb33be39db9a74db2a2033d5e7c9600414b1
yikezawa <y.ikezawa93@gmail.com>
2022-01-05 22:59:34 +0900

    mod

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -1,5 +1,18 @@
 class AsyncLogJob < ApplicationJob
-  queue_as :default
+  queue_as do
+    case self.arguments.first[:message]
+    when "to async_log"
+      :async_logs
+    else
+      :default
+    end
+  end
+
+  retry_on StandardError, wait: 5.seconds, attempts: 3
+
+  discard_on StandardError do |job, error|
+    SomeNotifier.push(error)
+  end
 
   def perform(message: "hello")
     AsyncLog.create!(message: message)
